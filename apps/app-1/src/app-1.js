37166ef29b4e8b69620ca49317d4d9519bcfc1fa
chobotek <mladek.milan@gmail.com>
2025-04-11 14:58:28 +0200

    chore:  workflow xx

--- apps/app-1/src/app-1.js
+++ apps/app-1/src/app-1.js
@@ -1,8 +1,7 @@
-import {divide, sum} from "ts-functions/src/math";
-
+import { divide, sum } from 'ts-functions/src/math'
 
 export const app1 = () => {
-    console.log("app2x")
-    console.log(sum(1, 2))
-    console.log(divide(6,4))
+  console.log('app2x')
+  console.log(sum(1, 2))
+  console.log(divide(6, 5))
 }

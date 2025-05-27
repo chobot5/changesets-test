de6b48a59fad412fbbe522f1b7efb1e55a1d8d69
chobotek <mladek.milan@gmail.com>
2025-05-27 14:39:05 +0200

    feat: app1 fu

--- apps/app-1/src/app-1.js
+++ apps/app-1/src/app-1.js
@@ -3,5 +3,5 @@ import { divide, sum } from 'ts-functions/src/math'
 export const app1 = () => {
   console.log('app2x')
   console.log(sum(1, 2))
-  console.log(divide(6, 20))
+  console.log(divide(6, 21))
 }
